65f34718a34ce2dfda0774f77b34f7872d9869b0
jian-lin <duoplay91095@gmail.com>
2020-09-11 12:25:53 +0800

    規則

---
+++ psrformat.php
@@ -0,0 +1,14 @@
+<?php
+    class PsrPratice
+    {
+        const PI="3.141596";
+        public $chooseBuy=1;
+        switch($chooseBuy)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+        }
+    }
+?>
